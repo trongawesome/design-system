ad2e1ed1c39d1aea05f640f0bf9ad38c32f0ea3b
Trong Nguyen <tantrongtt@gmail.com>
2019-12-27 11:33:07 +0800

    config 2

--- packages/example/src/gatsby-theme-carbon/components/Header.js
+++ packages/example/src/gatsby-theme-carbon/components/Header.js
@@ -3,7 +3,7 @@ import Header from 'gatsby-theme-carbon/src/components/Header';
 
 const CustomHeader = props => (
   <Header {...props}>
-    Gatsby theme&nbsp;<span>Carbon</span>
+    Carousell&nbsp;<span>Donut</span>
   </Header>
 );
 
