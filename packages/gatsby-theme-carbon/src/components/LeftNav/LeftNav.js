67dc4b752453fc0be12bbb83f7b451bfc041ed89
Trong Nguyen <tantrongtt@gmail.com>
2019-12-29 21:34:47 +0800

    light theme for navbar

--- packages/gatsby-theme-carbon/src/components/LeftNav/LeftNav.js
+++ packages/gatsby-theme-carbon/src/components/LeftNav/LeftNav.js
@@ -34,12 +34,12 @@ const LeftNav = props => {
         defaultExpanded
         aria-label="Side navigation"
         className={classnames({
-          [sideNavDark]: props.theme === 'dark' || props.homepage,
+          // [sideNavDark]: props.theme === 'dark' || props.homepage,
           'bx--side-nav--website': true,
-          'bx--side-nav--website--dark':
-            props.theme === 'dark' || props.homepage,
-          'bx--side-nav--website--light':
-            props.theme !== 'dark' && !props.homepage,
+          // 'bx--side-nav--website--dark':
+          //   props.theme === 'dark' || props.homepage,
+          'bx--side-nav--website--light': true,
+            // props.theme !== 'dark' && !props.homepage,
         })}
       >
         <SideNavItems>
